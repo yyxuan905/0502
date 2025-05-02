4d068ff34c95074fea71d0639401eb27711a1ebb
yyxuan905 <413730531@o365.tku.edu.tw>
2025-05-02 10:31:30 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,12 @@ function draw() {
   background('#edede9'); // 確保背景顏色持續更新
   let x = (width - capture.width) / 2; // 計算影像的水平居中位置
   let y = (height - capture.height) / 2; // 計算影像的垂直居中位置
-  image(capture, x, y, capture.width, capture.height); // 在畫布上繪製影像
+
+  push(); // 儲存當前畫布狀態
+  translate(width, 0); // 將畫布的原點移到右上角
+  scale(-1, 1); // 水平翻轉畫布
+  image(capture, x, y, capture.width, capture.height); // 在畫布上繪製翻轉後的影像
+  pop(); // 恢復畫布狀態
 }
 
 function windowResized() {
